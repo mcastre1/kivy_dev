52e991e999e1156904a16ce5362f1ecaef03ad0a
mcastre1 <miguelcastrejongal@gmail.com>
2023-11-09 06:02:16 -0700

    working player paddles and ball

--- src/main.py
+++ src/main.py
@@ -17,23 +17,51 @@ class PongBall(Widget):
 class PongGame(Widget):
     ball = ObjectProperty(None)
 
-    def serve_ball(self):
+    def serve_ball(self, vel=(4, 0)):
         self.ball.center = self.center
-        self.ball.velocity = Vector(4, 0).rotate(randint(0, 360))
+        self.ball.velocity = vel
 
     def update(self, dt):
         self.ball.move()
 
+        # bounce off paddles
+        self.player1.bounce_ball(self.ball)
+        self.player2.bounce_ball(self.ball)
+
         if (self.ball.y < 0) or (self.ball.top > self.height):
             self.ball.velocity_y *= -1
         
-        if(self.ball.x < 0) or (self.ball.right > self.width):
-            self.ball.velocity_x *= -1
+        # went off to a side to score point?
+        if self.ball.x < self.x:
+            self.player2.score += 1
+            self.serve_ball(vel=(4, 0))
+        if self.ball.right > self.width:
+            self.player1.score += 1
+            self.serve_ball(vel=(-4, 0))
 
+    def on_touch_move(self, touch):
+        if touch.x < self.width/3:
+            self.player1.center_y = touch.y
+        if touch.x > self.width - self.width/3:
+            self.player2.center_y = touch.y
+
+class PongPaddle(Widget):
+    score = NumericProperty(0)
+
+    def bounce_ball(self, ball):
+        if self.collide_widget(ball):
+            speedup = 1.1
+            offset = 0.02 * Vector(0, ball.center_y-self.center_y)
+            ball.velocity = speedup * (offset - ball.velocity)
+
+# This is what is being run when you run the main.py file
 class PongApp(App):
     def build(self):
+        # We create an instance of the "game"
         game = PongGame()
         game.serve_ball()
+
+        # Setup an scheduled event, which will update the balls movement
         Clock.schedule_interval(game.update, 1.0/60.0)
 
         return game
