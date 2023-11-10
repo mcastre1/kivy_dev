a70abfb3412f3c58bb9b3e7e5a5f31f7d9d84305
mcastre1 <miguelcastrejongal@gmail.com>
2023-11-10 05:31:17 -0700

    TextBox displays current operation, can clear text, and fraction gridlayout size is now correct.

--- imperialcalculator/main.py
+++ imperialcalculator/main.py
@@ -5,9 +5,27 @@ from kivy.uix.gridlayout import GridLayout
 
 class MainWidget(GridLayout):
     text = StringProperty('')
+    operation = []
+    left_side = ''
+    operator = ''
+    right_side = ''
 
+# Function called when Buttons in MainWidget widget are pressed
     def btn_pressed(self, btn):
-        self.text += f'{btn}'
+        if btn == 'c':
+            self.clear_text()
+        elif btn == '=':
+            pass
+        else:
+            self.operation.append(btn)
+
+        self.update_text()
+
+    def clear_text(self):
+        self.operation.clear()
+
+    def update_text(self):
+        self.text = ' '.join(self.operation)
 
 
 # Base Class of app, it should always inherit App
