7ace22ec3d2087a6e50b3b043bf435a2c014e868
mcastre1 <miguelcastrejongal@gmail.com>
2023-11-10 21:37:35 -0700

    Evaluation working properly, might need to check for edge cases.

--- imperialcalculator/main.py
+++ imperialcalculator/main.py
@@ -5,25 +5,36 @@ from kivy.uix.gridlayout import GridLayout
 
 class MainWidget(GridLayout):
     text = StringProperty('')
-    operation = []
-    left_side = ''
-    operator = ''
-    right_side = ''
+    operation = ['', '', '']
+    left_side = True
 
-# Function called when Buttons in MainWidget widget are pressed
+    # Function called when Buttons in MainWidget widget are pressed
     def btn_pressed(self, btn):
         if btn == 'c':
             self.clear_text()
-        elif btn == '=':
-            pass
-        else:
-            self.operation.append(btn)
+        elif btn == '=':  # Using the built-in function eval to evaluate an operation in string format
+            print(eval(' '.join(self.operation)))
+        elif btn == '+' or btn == '-':
+            if self.operation[0] == '':
+                self.operation[0] = '0'
+            self.operation[1] = btn
+            self.left_side = False
+
+        else:  # When a number is pressed
+            op = str(eval(btn))
+            if self.left_side:
+                self.operation[0] += f'.{op.split(".")[1]}' if '0' in op else op
+            else:
+                self.operation[2] += f'.{op.split(".")[1]}' if '0' in op else op
 
         self.update_text()
 
+    # Function called when operation list and display need to be cleared.
     def clear_text(self):
-        self.operation.clear()
+        self.operation = ['', '', '']
+        self.left_side = True
 
+    # Turns the operation list into a string and displays it on the main widget text box
     def update_text(self):
         self.text = ' '.join(self.operation)
 
