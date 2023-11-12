0ad224caeab0844291470ba84a9db50e1e9336e4
mcastre1 <miguelcastrejongal@gmail.com>
2023-11-11 17:28:52 -0700

    Evaluation of operation is somewhat working.

--- imperialcalculator/main.py
+++ imperialcalculator/main.py
@@ -6,25 +6,28 @@ from kivy.uix.gridlayout import GridLayout
 class MainWidget(GridLayout):
     text = StringProperty('')
     operation = []
-    operators = ['+', '-']
+    operators = ['+', '-']  # List of available and usable operators
 
     # Function called when Buttons in MainWidget widget are pressed
     def btn_pressed(self, btn):
         if btn == 'c':
             self.clear_operation()
         elif btn == '=':  # Using the built-in function eval to evaluate an operation in string format
-            print(''.join(self.operation))
+            self.evaluate()
         else:
             if '/' in btn:
                 if len(self.operation) > 0:
                     if not '/' in self.operation[-1]:
-                        self.operation.append(btn)
+                        if self.operation[-1].strip() in self.operators:
+                            self.operation.append(btn)
+                        else:
+                            self.operation.append(f' {btn}')
                 else:
                     self.operation.append(btn)
 
             elif btn in self.operators:
                 if len(self.operation) > 0:
-                    if not self.operation[-1] in self.operators:
+                    if not self.operation[-1].strip() in self.operators:
                         self.operation.append(f' {btn} ')
             else:
                 self.operation.append(btn)
@@ -39,9 +42,23 @@ class MainWidget(GridLayout):
     def update_text(self):
         self.text = ''.join(self.operation)
 
+    # Remove the last item in the list, if the list has more than one item.
     def backspace(self):
         if len(self.operation) > 0:
             self.operation.pop()
+        self.update_text()
+
+    # Translates fraction format numbers to actual decimals
+    def evaluate(self):
+        digit_op = []
+
+        for op in self.operation:
+            if '/' in op:  # Here is where we translate the fraction format number to decimal
+                digit_op.append(f'.{str(eval(op.strip())).split(".")[1]}')
+            else:  # Append anything else
+                digit_op.append(op.strip())
+
+        print(eval(''.join(digit_op)))
 
 
 # Base Class of app, it should always inherit App
