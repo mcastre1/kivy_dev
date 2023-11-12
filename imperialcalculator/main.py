24e6ab0d65bd14b411706d5404f35dd38569f43e
mcastre1 <miguelcastrejongal@gmail.com>
2023-11-11 17:03:51 -0700

    Added backspace button, and whole algorithm now uses a stack to make sure operands and operators are correct.

--- imperialcalculator/main.py
+++ imperialcalculator/main.py
@@ -5,38 +5,43 @@ from kivy.uix.gridlayout import GridLayout
 
 class MainWidget(GridLayout):
     text = StringProperty('')
-    operation = ['', '', '']
-    left_side = True
+    operation = []
+    operators = ['+', '-']
 
     # Function called when Buttons in MainWidget widget are pressed
     def btn_pressed(self, btn):
         if btn == 'c':
-            self.clear_text()
+            self.clear_operation()
         elif btn == '=':  # Using the built-in function eval to evaluate an operation in string format
-            print(eval(' '.join(self.operation)))
-        elif btn == '+' or btn == '-':
-            if self.operation[0] == '':
-                self.operation[0] = '0'
-            self.operation[1] = btn
-            self.left_side = False
+            print(''.join(self.operation))
+        else:
+            if '/' in btn:
+                if len(self.operation) > 0:
+                    if not '/' in self.operation[-1]:
+                        self.operation.append(btn)
+                else:
+                    self.operation.append(btn)
 
-        else:  # When a number is pressed
-            op = str(eval(btn))
-            if self.left_side:
-                self.operation[0] += f'.{op.split(".")[1]}' if '0' in op else op
+            elif btn in self.operators:
+                if len(self.operation) > 0:
+                    if not self.operation[-1] in self.operators:
+                        self.operation.append(f' {btn} ')
             else:
-                self.operation[2] += f'.{op.split(".")[1]}' if '0' in op else op
+                self.operation.append(btn)
 
         self.update_text()
 
     # Function called when operation list and display need to be cleared.
-    def clear_text(self):
-        self.operation = ['', '', '']
-        self.left_side = True
+    def clear_operation(self):
+        self.operation.clear()
 
     # Turns the operation list into a string and displays it on the main widget text box
     def update_text(self):
-        self.text = ' '.join(self.operation)
+        self.text = ''.join(self.operation)
+
+    def backspace(self):
+        if len(self.operation) > 0:
+            self.operation.pop()
 
 
 # Base Class of app, it should always inherit App
