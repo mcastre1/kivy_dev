d9fca998e404a2a35f98e4e218472519b0ba5cea
mcastre1 <miguelcastrejongal@gmail.com>
2023-11-12 09:57:23 -0700

    Fixed bug where first index of self.operation would be a whole measurement and would crash the app on equal button press.

--- imperialcalculator/main.py
+++ imperialcalculator/main.py
@@ -8,6 +8,28 @@ class MainWidget(GridLayout):
     operation = []
     operators = ['+', '-', '*', '/']  # List of available and usable operators
 
+    # Available fractions
+    fractions = {'.0625': '1/16',
+                 '.125': '1/8',
+                 '.1875': '3/16',
+                 '.25': '1/4',
+                 '.3125': '5/16',
+                 '.375': '3/8',
+                 '.4375': '7/16',
+                 '.5': '1/2',
+                 '.5625': '9/16',
+                 '.625': '5/8',
+                 '.6875': '11/16',
+                 '.75': '3/4',
+                 '.8125': '13/16',
+                 '.875': '7/8',
+                 '.9375': '15/16'}
+
+    # Colors
+    operand_bg = .08, .5, .8, 1
+    equal_bg = .26, .78, .08, 1
+    backspace_bg = .6, .5, .05, 1
+
     # Function called when Buttons in MainWidget widget are pressed
     def btn_pressed(self, btn):
         if btn == 'c':
@@ -61,14 +83,39 @@ class MainWidget(GridLayout):
             print(op.strip())
             if '/' in op and len(op.strip()) > 2:  # Here is where we translate the fraction format number to decimal
 
+                print(f'here. {self.operation}')
                 digit_op.append(f'.{str(eval(op.strip())).split(".")[1]}')
             else:  # Append anything else
                 digit_op.append(op.strip())
 
         self.clear_operation()
-        self.operation.append(str(eval(''.join(digit_op))))
+
+        val = str(eval(''.join(digit_op)))
+        print(val)
+
+        val = self.convert_fraction(val)
+        val_split = val.split(' ')
+        print(val_split)
+
+        # Making sure returned value is in the same format as _, _, _ first self.operation format
+        if len(val_split) == 2:
+            self.operation.append(val_split[0])
+            self.operation.append(' ' + val_split[1])
+        else:
+            self.operation.append(val)
         self.update_text()
-        print(eval(''.join(digit_op)))
+
+    # Converts decimal points into available fractions
+    def convert_fraction(self, val):
+        if '.' in val:
+            dec = f'.{val.split(".")[1]}'
+            if dec in self.fractions.keys():
+                if not val.split(".")[0] == 0:
+                    val = val.split(".")[0] + ' ' + self.fractions[dec]
+                else:
+                    val = self.fractions[dec]
+
+        return val
 
 
 # Base Class of app, it should always inherit App
